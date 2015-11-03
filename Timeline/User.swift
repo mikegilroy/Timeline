666db2f9d9d088bf8ac19fa012d19ca10bf9217c
Mike Gilroy <mjgilroy@me.com>
2015-11-03 14:51:09 -0700

    Implement Model: User

---
+++ Timeline/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  Timeline
+//
+//  Created by Mike Gilroy on 03/11/2015.
+//  Copyright © 2015 Mike Gilroy. All rights reserved.
+//
+
+import Foundation
+
+struct User: Equatable {
+    
+    var username: String
+    var bio: String? = nil
+    var url: String? = nil
+    var userID: String
+    
+    init(username: String, bio: String, url: String, userID: String) {
+        
+        self.username = username
+        self.bio = bio
+        self.url = url
+        self.userID = userID
+    }
+    
+}
+
+
+// Equatable protocol
+func == (user1: User, user2: User) -> Bool {
+    return ((user1.username == user2.username) && (user1.userID == user2.userID))
+}
+
+
+
