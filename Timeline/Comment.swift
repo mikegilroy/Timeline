b1cca18921f0d94840acba11ac7e80ae0abfcf77
Mike Gilroy <mjgilroy@me.com>
2015-11-03 15:03:34 -0700

    Implement Model: Comment

--- Timeline/Comment.swift
+++ Timeline/Comment.swift
@@ -8,19 +8,23 @@
 
 import Foundation
 
-struct Comment {
+struct Comment: Equatable {
     
-//    Comment
-//    
-//    Create a 'Comment' model struct that will hold a username, text, and reference to the parent Post.
-//    
-//    Create a Comment.swift class file and define a new Comment struct
-//    Add properties for username, text, postIdentifier, and optional identifier
-//    Add a memberwise initializer that takes parameters for each property
-//    Set a default parameter nil for optional properties
-//    Implement the Equatable protocol by comparing the username and identifier
+    var username: String
+    var text: String
+    var postID: String
+    var commentID: String
     
-    
-    
-    
+    init(username: String, text: String, postID: String, commentID: String) {
+        
+        self.username = username
+        self.text = text
+        self.postID = postID
+        self.commentID = commentID
+    }
+}
+
+// Equatable protocol
+func == (lhs: Comment, rhs: Comment) -> Bool {
+    return ((lhs.username == rhs.username) && (lhs.commentID == rhs.commentID))
 }
