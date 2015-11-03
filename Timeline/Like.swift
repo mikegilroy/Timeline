c4896984f7bd594b4725325315fa66bf528ecae5
Mike Gilroy <mjgilroy@me.com>
2015-11-03 15:05:48 -0700

    Implement model: Like

--- Timeline/Like.swift
+++ Timeline/Like.swift
@@ -8,21 +8,21 @@
 
 import Foundation
 
-struct Like {
+struct Like: Equatable {
     
-//    Like
-//    
-//    Create a 'Like' model struct that will hold a username, and reference to the parent Post.
-//    
-//    Create a Like.swift class file and define a new Like struct
-//    Add properties for username, postIdentifier, and optional identifier
-//    Add a memberwise initializer that takes parameters for each property
-//    Set a default parameter nil for optional properties
-//    Implement the Equatable protocol by comparing the username and identifier
-//    The model objects will later conform to a FirebaseType protocol that will ease working with Firebase. You will add the required properties and functions at that point.
+    var username: String
+    var postID: String
+    var likeID: String
     
-    
-    
-    
-    
+    init(username: String, postID: String, likeID: String) {
+        
+        self.username = username
+        self.postID = postID
+        self.likeID = likeID
+    }
+}
+
+// Equatable protocol
+func == (lhs: Like, rhs: Like) -> Bool {
+    return ((lhs.username == rhs.username) && (lhs.likeID == rhs.likeID))
 }
