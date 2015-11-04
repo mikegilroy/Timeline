9db5d03c30baebf7773cfad708187494e0c6cfde
Mike Gilroy <mjgilroy@me.com>
2015-11-03 19:20:44 -0700

    Signup / Login View: Class implementation

--- Timeline/LoginSignupViewController.swift
+++ Timeline/LoginSignupViewController.swift
@@ -10,26 +10,106 @@ import UIKit
 
 class LoginSignupViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Outlets
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var bioTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    // MARK: Properties
+    
+    enum ViewMode {
+        case Login
+        case Signup
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    var mode: ViewMode = .Signup
+    
+    var fieldsAreValid: Bool {
+        switch mode {
+        case .Login:
+            return !((emailTextField.text!.isEmpty) || (passwordTextField.text!.isEmpty))
+        case .Signup:
+            return !((emailTextField.text!.isEmpty) || (passwordTextField.text!.isEmpty) || (usernameTextField.text!.isEmpty))
+        }
     }
-    */
+    
+    
+    // MARK: Actions
+    
+    @IBAction func actionButtonTapped(sender: AnyObject) {
+        if fieldsAreValid {
+            switch mode {
+            case .Signup:
+                UserController.createUser(emailTextField.text!, username: usernameTextField.text!, password: passwordTextField.text!, bio: bioTextField?.text, url: urlTextField?.text, completion: { (success, user) -> Void in
+                    if success {
+                        // dismiss view controller
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                       
+                    }
+                })
+                
+            case .Login:
+                UserController.authenticateUser(emailTextField.text!, password: passwordTextField.text!, completion: { (success, user) -> Void in
+                    if success {
+                        // dismiss view controller
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    } else {
+                       
+                    }
+                })
+            }
+        } else {
+            switch mode {
+            case .Signup:
+                 self.presentValidationAlertWithTitle("Missing something?", text: "You'll need to enter a valid username, email and password before we can sign you up!")
+            case .Login:
+                self.presentValidationAlertWithTitle("We don't recognise that", text: "Please make sure you've entered the correct email and password")
+            }
+        }
+    }
+    
+    
+    
+    // MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViewForMode(mode)
+    }
+    
+    
+    func updateViewForMode(mode: ViewMode) {
+        switch mode {
+        case .Signup:
+            loginButton.setTitle("Sign up", forState: .Normal)
+            usernameTextField.hidden = false
+            emailTextField.hidden = false
+            passwordTextField.hidden = false
+            bioTextField.hidden = false
+            urlTextField.hidden = false
+            
+        case .Login:
+            loginButton.setTitle("Login", forState: .Normal)
+            usernameTextField.hidden = true
+            emailTextField.hidden = false
+            passwordTextField.hidden = false
+            bioTextField.hidden = true
+            urlTextField.hidden = true
+        }
+    }
 
+    
+    func presentValidationAlertWithTitle(title: String, text: String) {
+        let alert = UIAlertController(title: title, message: text, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
 }
