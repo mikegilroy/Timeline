2fe843947f08c01ee30c7ddafcfdbf590b16be18
Mike Gilroy <mjgilroy@me.com>
2015-11-09 15:30:04 -0700

    Implemented Firebase controller functions

---
+++ FirebaseController.swift
@@ -0,0 +1,40 @@
+//
+//  FirebaseController.swift
+//  Timeline
+//
+//  Created by Mike Gilroy on 09/11/2015.
+//  Copyright © 2015 Mike Gilroy. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseController {
+    
+    static let base = Firebase(url: "https://instatweed.firebaseio.com/")
+    
+    static func dataAtEndpoint(endpoint: String, completion: (data: AnyObject?) -> Void) {
+        
+        let ref = base.childByAppendingPath(endpoint)
+        ref.observeSingleEventOfType(.Value, withBlock: { (snapshot) -> Void in
+            if snapshot.value is NSNull {
+                completion(data: nil)
+            } else {
+                completion(data: snapshot.value)
+            }
+        })
+    }
+    
+    
+    static func observeDataAtEndpoint(endpoint: String, completion: (data: AnyObject?) -> Void) {
+        let ref = base.childByAppendingPath(endpoint)
+        ref.observeEventType(.Value, withBlock: { (snapshot) -> Void in
+            if snapshot.value is NSNull {
+                completion(data: nil)
+            } else {
+                completion(data: snapshot.value)
+            }
+        })
+    }
+    
+}
